fde668cfcc8e2b79b26b4792759e098e3e8332df
kremenayo <kremena.yo@gmail.com>
2020-12-10 15:50:27 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -21,13 +21,24 @@ stylesheet = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=stylesheet)
 server = app.server
 
-text = ''' This dashboard uses 943 recipeis from allrecipes.com. This ....
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus scelerisque efficitur magna, id sagittis augue egestas vel. Vivamus consectetur turpis nec ornare lacinia. Pellentesque sed convallis erat. Aliquam ut est nisl. Etiam pretium fermentum diam, eu consectetur elit gravida vel.
-'''
+text = '''This dashboard summarized the information of … cooking recipes, 
+scraped from allrecipes.com. By using this dashboard, a user can easily choose 
+from all available recipes those that match three search criteria:'''
 
-text_filters = '''You can use these filters to select recipes to load in the table below.
-If there are not any recipes matching your filters an error message will be printet on 
-the left hand-side indicating that there are any matching results.'''
+text_l1 = '''- Recipe category'''
+text_l2 = '''- The maximum time to cook a recipe, specified in minutes. This time includes 
+the recipe preparation time, along with the cooking time and any additional time needed.'''
+text_l3 = '''- The maximum number of calories per serving.'''
+
+text_filters = '''Please use these filters to select recipes to load on the dashboard. 
+The information about the selected recipes is presented in the table below, 
+while the pie chart on the right shows the recipe types of the selected recipes. 
+If there are no recipes matching search criteria, an error message will be printed.
+The cooking recipes in the table are sorted by the number of reviews, showing the most 
+reviewed recipes on the top.'''
+
+text_ref = '''Here is a list of data sources and references used in this course project.'''
+
 
 file = 'https://raw.githubusercontent.com/kremenayo/cooking-recipe-dashboard/main/recipes.csv'
 df = pd.read_csv(file, index_col=0)
@@ -46,74 +57,91 @@ categories = df['category_main'].unique().tolist()
 PAGE_SIZE = 10
 
 app.layout = html.Div([
-    html.H1('Cooking Recipe Dashboard', style={'textAlign': 'left'}),
-    html.Blockquote('MA 705 Course Project by Kremena Ivanov', style={'textAlign': 'left'}),
-    html.B('What is this dashboard about?'),
-    html.Section(text, style={'textAlign': 'left'}),
-    html.B('How to use this dashboard?'),
-    html.Section(text, style={'textAlign': 'left'}),
-    html.Br(),
-    html.Hr(),
-    html.Div([html.Div(text_filters),
-              html.Br(),
-              
-              html.Label("Select category to display: "),
-              dcc.Dropdown(id='filter_dropdown',
-                           placeholder='Select category...',
-                           options=[{'label':c, 'value':c} for c in categories],
-                           value='Dessert Recipes'),
-              html.Br(),
-              html.Label("Select total time (in minutes): "),
-              dcc.Input(placeholder='Enter time in minutes...',
-                                   id='ttl_time', type='number', value=20),
-              html.Br(),
-              html.Br(),
-              html.Label("Select maximum calories: "),
-              dcc.Input(placeholder='Enter max calories...',
-                                   id='cals', type='number', value=500)],
-             style={'width': '39%', 'display': 'inline-block', 'vertical-align' : 'top'}
-             ),
     html.Div([],
-             style={'width': '14%', 'display': 'inline-block'}),
-    html.Div([dcc.Graph(id='fig_pie')],
-             style={'width': '45%', 'display': 'inline-block', 'vertical-align' : 'top'}),
-    html.H3('Selected Recipes', style={'textAlign': 'left'}), 
-    html.Br(),
-    html.Div(className="row",
-             children=[
-                 html.Div(dash_table.DataTable(id='table-paging-with-graph',
-                                               columns=[dict(name='Recipe name', id='name', type='text'),
-                                                        dict(name='Sub-category', id='category_sub', type='text'),
-                                                        dict(name='# Servings', id='servings', type='text'),
-                                                        dict(name='Yield', id='yield', type='text'),
-                                                        dict(name='# Reviews', id='reviews', type='text'),
-                                                        dict(name='Avg. rating (0-5)', id='stars', type='text'),
-                                                        dict(name='Prep time', id='prep_time_min', type='text'),
-                                                        dict(name='Cook time', id='cook_time_min', type='text'),
-                                                        dict(name='Total time', id='ttl_time_min', type='text'),
-                                                        dict(name='Link', id='link', type='text', presentation='markdown')
-                                                        ],
-                                               page_current=0,
-                                               page_size=PAGE_SIZE,
-                                               page_action='custom',
-                                               filter_action='custom',
-                                               filter_query='',
-                                               sort_action='custom',
-                                               sort_mode='multi',
-                                               sort_by=[]
-                                               ),
-                          style={'width': '99%', 'display': 'inline-block', 'overflowY': 'scroll'}
-                          )
-
-                 ]),
-    html.Hr(),
-    html.B('References:'),
-    html.Li(html.A('https://www.allrecipes.com/')),
-    html.Li(html.A('https://dash.plotly.com/datatable/callbacks')),
-    html.Li(html.A('https://plotly.com/python/pie-charts/')),
-    html.Li(html.A('https://github.com/plotly/dash-table/issues/222')),
-    html.Br(),
-    html.Footer('This project was created ...'),
+             style={'width': '5%', 'display': 'inline-block'}),
+    html.Div([
+        html.Div([html.Br(),
+            html.H1('Cooking Recipe Dashboard', style={'color':'#003B73', 'textAlign': 'left'}),
+            html.Blockquote('MA 705 Course Project by Kremena Ivanov', style={'textAlign': 'left'}),
+            html.B('What is this dashboard about?'),
+            html.Section(text, style={'textAlign': 'left'}),
+            html.Section(text_l1),
+            html.Section(text_l2),
+            html.Section(text_l3),
+            ], style={'width': '90%', 'display': 'inline-block', 'vertical-align' : 'top'}),
+        html.Div([],
+                 style={'width': '4%', 'display': 'inline-block'}),  
+        #html.B('How to use this dashboard?'),
+        #html.Section(text, style={'textAlign': 'left'}),
+        html.Br(),
+        html.Hr(),
+        
+        html.Div([html.B('How to use this dashboard?'),
+                  html.Div(text_filters),
+                  html.Br(),
+                  
+                  html.Label("Select a category to display: "),
+                  dcc.Dropdown(id='filter_dropdown',
+                               placeholder='Select category...',
+                               options=[{'label':c, 'value':c} for c in categories],
+                               value='Dessert Recipes'),
+                  html.Br(),
+                  html.Label("Select total time (prep & cook time) in minutes: "),
+                  dcc.Input(placeholder='Enter time in minutes...',
+                                       id='ttl_time', type='number', value=20),
+                  html.Br(),
+                  html.Br(),
+                  html.Label("Select maximum calories per serving: "),
+                  dcc.Input(placeholder='Enter max calories...',
+                                       id='cals', type='number', value=500)],
+                 style={'width': '40%', 'display': 'inline-block', 'vertical-align' : 'top'}
+                 ),
+        html.Div([],
+                 style={'width': '5%', 'display': 'inline-block'}),
+        html.Div([html.H5(id='message_selected'), dcc.Graph(id='fig_pie')],
+                 style={'width': '55%', 'display': 'inline-block', 'vertical-align' : 'top'}),
+        
+        html.H3('Selected Recipes', style={'textAlign': 'left'}),
+        html.Div(className="row",
+                 children=[
+                     html.Div(dash_table.DataTable(id='table-paging-with-graph',
+                                                   columns=[dict(name='Recipe name', id='name', type='text'),
+                                                            dict(name='Sub-category', id='category_sub', type='text'),
+                                                            dict(name='# Servings', id='servings', type='text'),
+                                                            dict(name='Yield', id='yield', type='text'),
+                                                            dict(name='# Reviews', id='reviews', type='text'),
+                                                            dict(name='Avg. rating (0-5)', id='stars', type='text'),
+                                                            dict(name='Prep time', id='prep_time_min', type='text'),
+                                                            dict(name='Cook time', id='cook_time_min', type='text'),
+                                                            dict(name='Total time', id='ttl_time_min', type='text'),
+                                                            dict(name='Link', id='link', type='text', presentation='markdown')
+                                                            ],
+                                                   page_current=0,
+                                                   page_size=PAGE_SIZE,
+                                                   page_action='custom',
+                                                   filter_action='custom',
+                                                   filter_query='',
+                                                   sort_action='custom',
+                                                   sort_mode='multi',
+                                                   sort_by=[]
+                                                   ),
+                              style={'width': '99%', 'display': 'inline-block', 'overflowY': 'scroll'}
+                              )
+    
+                     ]),
+        html.Hr(),
+        html.B('References:'),
+        html.Div(text_ref),
+        html.Section(['- Cooking recipe information: ', html.A('https://www.allrecipes.com/')]),
+        html.Section(['- Dash Plotly Callbacks: ',html.A('https://dash.plotly.com/datatable/callbacks')]),
+        html.Section(['- Dash Plotly Pie Chart: ', html.A('https://plotly.com/python/pie-charts/')]),
+        html.Section(['- Dash Plotly Url Formatting: ', html.A('https://github.com/plotly/dash-table/issues/222')]),
+        html.Br(),
+        html.Footer('This project was created ..., December 2020, Kremena Ivanov.')
+        ], style={'width': '90%', 'display': 'inline-block'}),
+    
+    html.Div([],
+             style={'width': '5%', 'display': 'inline-block'}),
 ])
 
 
@@ -213,16 +241,39 @@ def update_graph(category, time, cals):
     if dff_crosstab.empty:
         data = pd.DataFrame(index=['No recipes to show'])
         data['recipes'] = 0
-        fig = px.pie(data, values=data['recipes'], names=data.index, title='No recipes to show. Please choose different parameters')
+        fig = px.pie(data, values=data['recipes'], names=data.index)
+    # title='No recipes to show. Please choose different parameters.'
     else:
-        fig = px.pie(dff_crosstab, values=dff_crosstab['recipes'], names=dff_crosstab.index, title='Recipe sub-categories')
-    # fig = px.bar(k, y='recipes', x=k.index, text='recipes')
-    # fig.update_traces(texttemplate='%{text:.2s}', textposition='outside')
-    # fig.update_layout(uniformtext_minsize=8, uniformtext_mode='hide')
-
-
+        fig = px.pie(dff_crosstab, values=dff_crosstab['recipes'], names=dff_crosstab.index)
+    # title='Recipe sub-categories'
     return fig
 
+
+@app.callback(
+    Output(component_id='message_selected', component_property='children'),
+    Input('filter_dropdown', 'value'),
+    Input('ttl_time', 'value'),
+    Input('cals', 'value'))
+
+def update_graph(category, time, cals):
+    # category = 'Dessert Recipes'
+    # time = 100
+    # cals = 400
+    
+    dff = df[(df['category_main']==category) & (df['ttl_time_min'] <= time) & (df['calories'] <= cals)]
+    dff_crosstab = dff.groupby('category_sub')
+    dff_crosstab = dff_crosstab['category_sub'].count()
+    dff_crosstab = dff_crosstab.to_frame()
+    dff_crosstab.columns = ['recipes']
+    n_recipes = dff_crosstab.sum().values[0]
+    if dff_crosstab.empty:
+        message ='No recipes to show. Please choose a different set of parameters.'
+    else:
+        if n_recipes > 1:
+            message = 'There are '+str(n_recipes)+' recipes matching your search criteria.'
+        elif n_recipes == 1:
+            message = 'There is 1 recipe matching your search criteria.'
+    return message
 
 
 if __name__ == '__main__':
